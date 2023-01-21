6134fdcb5821aa0e16842005e4743d3299497cc0
Tien Khoa Nguyen <Tyson229>
2023-01-21 09:03:12 +0700

    patch request added for job

--- src/main/java/nology/io/JobAssignmentAPI/Job/JobService.java
+++ src/main/java/nology/io/JobAssignmentAPI/Job/JobService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
@@ -21,15 +22,16 @@ public class JobService {
   private TempService tempService;
 
   public Job create(JobCreateDTO data) {
-    if(data.getStartDate().isAfter(data.getEndDate()))
+    if (data.getStartDate().isAfter(data.getEndDate()))
       throw new RuntimeException("Invalid date input");
+
     String cleanedName = data.getName().trim();
     Job newJob = new Job(cleanedName, data.getStartDate(), data.getEndDate());
-    
+
     // Fetch temp data from db
     Long tempId = data.getTemp();
 
-    if(tempId != null)
+    if (tempId != null)
       assignTemp(newJob, tempId);
 
     this.jobRepository.save(newJob);
@@ -61,25 +63,57 @@ public class JobService {
 
     LocalDate startDate = data.getStartDate();
     LocalDate endDate = data.getEndDate();
-    
-    // When both new dates exist
-    if(startDate != null && endDate != null && startDate.isAfter(endDate))
-      throw new RuntimeException("Invalid date");
-    else if(startDate != null)
-      job.setStartDate(startDate);
-    if(endDate != null)
-      job.setEndDate(endDate);
+    switch (validateDate(startDate, endDate, job.getStartDate(), job.getEndDate())) {
+      case 0:  
+        throw new RuntimeException("Invalid dates"); 
+        
+      case 1: 
+        job.setStartDate(startDate);
+        job.setEndDate(endDate);
+        break;
+      
+      case 2:
+        job.setStartDate(startDate);
+        break;
 
-    return job;
+      case 3:
+        job.setEndDate(endDate);
+        break;
+      
+      default: break;
+    }
+    
+
+  return job;
 
   }
 
   private Job assignTemp(Job job, Long tempId) {
-    Temp temp = tempService.findOne(tempId).orElseThrow(() -> new RuntimeException("Temp not found"));  
-      // Set new job for that temp from the data variable
+    Temp temp = tempService.findOne(tempId).orElseThrow(() -> new RuntimeException("Temp not found"));
+    // Set new job for that temp from the data variable
     job.setTemp(temp);
     temp.getJobs().add(job);
 
     return job;
+  }
+
+  private int validateDate(LocalDate newStartDate, LocalDate newEndDate, LocalDate currentStartDate, LocalDate currentEndDate) {
+    // Validate dates
+    if(newStartDate != null && newEndDate != null) {
+      if(newStartDate.isAfter(newEndDate))
+        return 0;
+      return 1;
+    }
+    if(newStartDate != null) {
+      if(newStartDate.isAfter(currentEndDate))
+        return 0;
+      return 2;
+    } 
+    if(newEndDate != null) {
+      if(newEndDate.isBefore(currentStartDate)) 
+        return 0;
+      return 3;
+    }
+    return -1;
   }
 }
